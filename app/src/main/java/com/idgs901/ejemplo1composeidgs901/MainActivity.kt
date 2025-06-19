ccf6854e13ad8003a96d1124a8a89f09bff554fb
IDGS-801-22000977 <79349@alumnos.utleon.edu.mx>
2025-06-18 18:37:00 -0600

    Tarjetas terminadas 18/06/2025

--- app/src/main/java/com/idgs901/ejemplo1composeidgs901/MainActivity.kt
+++ app/src/main/java/com/idgs901/ejemplo1composeidgs901/MainActivity.kt
@@ -6,36 +6,49 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 
 private val tarjetas: List<PersonajeTarjeta> = listOf(
-PersonajeTarjeta("Goku","El protagonista de la serie, conocido por su gran poder y personalidad amigable. Originalmente enviado a la Tierra como un infante volador con la misión de conquistarla. Sin embargo, el caer por un barranco le proporcionó un brutal golpe que si bien casi lo mata, este alteró su memoria y anuló todos los instintos violentos de su especie, lo que lo hizo crecer con un corazón puro y bondadoso, pero conservando todos los poderes de su raza. No obstante, en la nueva continuidad de Dragon Ball se establece que él fue enviado por sus padres a la Tierra con el objetivo de sobrevivir a toda costa a la destrucción de su planeta por parte de Freeza. Más tarde, Kakarot, ahora conocido como Son Goku, se convertiría en el príncipe consorte del monte Fry-pan y líder de los Guerreros Z, así como el mayor defensor de la Tierra y del Universo 7, logrando mantenerlos a salvo de la destrucción en innumerables ocasiones, a pesar de no considerarse a sí mismo como un héroe o salvador."),
-    PersonajeTarjeta("Vegeta","Príncipe de los Saiyans, inicialmente un villano, pero luego se une a los Z Fighters. A pesar de que a inicios de Dragon Ball Z, Vegeta cumple un papel antagónico, poco después decide rebelarse ante el Imperio de Freeza, volviéndose un aliado clave para los Guerreros Z. Con el paso del tiempo llegaría a cambiar su manera de ser, optando por permanecer y vivir en la Tierra para luchar a su lado contra las inminentes adversidades que superar. Junto con Piccolo, él es de los antiguos enemigos de Goku que ha evolucionando al pasar de ser un villano y antihéroe, a finalmente un héroe a lo largo del transcurso de la historia, convirtiéndose así en el deuteragonista de la serie."),
-    PersonajeTarjeta("Picolo","Es un namekiano que surgió tras ser creado en los últimos momentos de vida de su padre, siendo su actual reencarnación. Aunque en un principio fue el archienemigo de Son Goku, con el paso del tiempo fue haciéndose menos malvado hasta finalmente convertirse en un ser bondadoso y miembro de los Guerreros Z. A través del tiempo, también comenzó a tomarle cariño a su discípulo Son Gohan, a quien veía como una especie de vástago y formando un lazo de amistad con este."),
-    PersonajeTarjeta("Bulma","Bulma es la protagonista femenina de la serie manga Dragon Ball y sus adaptaciones al anime Dragon Ball, Dragon Ball Z, Dragon Ball Super y Dragon Ball GT. Es hija del Dr. Brief y su esposa Panchy, hermana menor de Tights y una gran amiga de Son Goku con quien inicia la búsqueda de las Esferas del Dragón. En Dragon Ball Z tuvo a Trunks, primogénito de quien sería su esposo Vegeta, a su hija Bra[3] y su hijo adulto del tiempo alterno Trunks del Futuro Alternativo."),
-    PersonajeTarjeta("Freezer","Freezer es el tirano espacial y el principal antagonista de la saga de Freezer."),
-    PersonajeTarjeta("Zarbon", "Zarbon es uno de los secuaces de Freezer y un luchador poderoso."),
-    PersonajeTarjeta("Dodoria","Dodoria es otro secuaz de Freezer conocido por su brutalidad."),
-    PersonajeTarjeta("Gohan","Son Gohanda en su tiempo en España, o simplemente Gohan en Hispanoamérica, es uno de los personajes principales de los arcos argumentales de Dragon Ball Z, Dragon Ball Super y Dragon Ball GT. Es un mestizo entre saiyano y humano terrícola. Es el primer hijo de Son Goku y Chi-Chi, hermano mayor de Son Goten, esposo de Videl y padre de Pan."),
-    PersonajeTarjeta("Krilin","Amigo cercano de Goku y guerrero valiente, es un personaje del manga y anime de Dragon Ball. Es uno de los principales discípulos de Kame-Sen'nin, Guerrero Z, y el mejor amigo de Son Goku. Es junto a Bulma uno de los personajes de apoyo principales de Dragon Ball, Dragon Ball Z y Dragon Ball Super. Aparece en Dragon Ball GT como personaje secundario. En el Arco de Majin Boo, se retira de las artes marciales, optando por formar una familia, como el esposo de la Androide Número 18 y el padre de Marron."),
-    PersonajeTarjeta("Tenshinhan","Maestro de las artes marciales y miembro de los Z Fighters. Es un personaje que aparece en el manga y en el anime de Dragon Ball, Dragon Ball Z, Dragon Ball GT y posteriormente en Dragon Ball Super.")
+PersonajeTarjeta("goku_normal","El protagonista de la serie, conocido por su gran poder y personalidad amigable. Originalmente enviado a la Tierra como un infante volador con la misión de conquistarla. Sin embargo, el caer por un barranco le proporcionó un brutal golpe que si bien casi lo mata, este alteró su memoria y anuló todos los instintos violentos de su especie, lo que lo hizo crecer con un corazón puro y bondadoso, pero conservando todos los poderes de su raza. No obstante, en la nueva continuidad de Dragon Ball se establece que él fue enviado por sus padres a la Tierra con el objetivo de sobrevivir a toda costa a la destrucción de su planeta por parte de Freeza. Más tarde, Kakarot, ahora conocido como Son Goku, se convertiría en el príncipe consorte del monte Fry-pan y líder de los Guerreros Z, así como el mayor defensor de la Tierra y del Universo 7, logrando mantenerlos a salvo de la destrucción en innumerables ocasiones, a pesar de no considerarse a sí mismo como un héroe o salvador."),
+    PersonajeTarjeta("vegeta_normal","Príncipe de los Saiyans, inicialmente un villano, pero luego se une a los Z Fighters. A pesar de que a inicios de Dragon Ball Z, Vegeta cumple un papel antagónico, poco después decide rebelarse ante el Imperio de Freeza, volviéndose un aliado clave para los Guerreros Z. Con el paso del tiempo llegaría a cambiar su manera de ser, optando por permanecer y vivir en la Tierra para luchar a su lado contra las inminentes adversidades que superar. Junto con Piccolo, él es de los antiguos enemigos de Goku que ha evolucionando al pasar de ser un villano y antihéroe, a finalmente un héroe a lo largo del transcurso de la historia, convirtiéndose así en el deuteragonista de la serie."),
+    PersonajeTarjeta("picolo_normal","Es un namekiano que surgió tras ser creado en los últimos momentos de vida de su padre, siendo su actual reencarnación. Aunque en un principio fue el archienemigo de Son Goku, con el paso del tiempo fue haciéndose menos malvado hasta finalmente convertirse en un ser bondadoso y miembro de los Guerreros Z. A través del tiempo, también comenzó a tomarle cariño a su discípulo Son Gohan, a quien veía como una especie de vástago y formando un lazo de amistad con este."),
+    PersonajeTarjeta("bulma","Bulma es la protagonista femenina de la serie manga Dragon Ball y sus adaptaciones al anime Dragon Ball, Dragon Ball Z, Dragon Ball Super y Dragon Ball GT. Es hija del Dr. Brief y su esposa Panchy, hermana menor de Tights y una gran amiga de Son Goku con quien inicia la búsqueda de las Esferas del Dragón. En Dragon Ball Z tuvo a Trunks, primogénito de quien sería su esposo Vegeta, a su hija Bra[3] y su hijo adulto del tiempo alterno Trunks del Futuro Alternativo."),
+    PersonajeTarjeta("freezer","Freezer es el tirano espacial y el principal antagonista de la saga de Freezer."),
+    PersonajeTarjeta("zarbon", "Zarbon es uno de los secuaces de Freezer y un luchador poderoso."),
+    PersonajeTarjeta("dodoria","Dodoria es otro secuaz de Freezer conocido por su brutalidad."),
+    PersonajeTarjeta("gohan","Son Gohanda en su tiempo en España, o simplemente Gohan en Hispanoamérica, es uno de los personajes principales de los arcos argumentales de Dragon Ball Z, Dragon Ball Super y Dragon Ball GT. Es un mestizo entre saiyano y humano terrícola. Es el primer hijo de Son Goku y Chi-Chi, hermano mayor de Son Goten, esposo de Videl y padre de Pan."),
+    PersonajeTarjeta("krilin_universo7","Amigo cercano de Goku y guerrero valiente, es un personaje del manga y anime de Dragon Ball. Es uno de los principales discípulos de Kame-Sen'nin, Guerrero Z, y el mejor amigo de Son Goku. Es junto a Bulma uno de los personajes de apoyo principales de Dragon Ball, Dragon Ball Z y Dragon Ball Super. Aparece en Dragon Ball GT como personaje secundario. En el Arco de Majin Boo, se retira de las artes marciales, optando por formar una familia, como el esposo de la Androide Número 18 y el padre de Marron."),
+    PersonajeTarjeta("tenshinhan_universo7","Maestro de las artes marciales y miembro de los Z Fighters. Es un personaje que aparece en el manga y en el anime de Dragon Ball, Dragon Ball Z, Dragon Ball GT y posteriormente en Dragon Ball Super.")
 )
 data class PersonajeTarjeta(val title:String, val body:String)
 
@@ -45,7 +58,7 @@ class MainActivity : ComponentActivity() {
         //enableEdgeToEdge()
         setContent {
             Ejemplo1ComposeIDGS901Theme {
-            SaludoCard("Android","personaje")
+
                 Tarjeta(tarjetas)
 
         }
@@ -53,70 +66,107 @@ class MainActivity : ComponentActivity() {
 }
 }
 @Composable
-fun Tarjeta(personajes:List<PersonajeTarjeta>){
+fun Tarjeta(personajes: List<PersonajeTarjeta>){
     LazyColumn {
         items(personajes){ personaje ->
+
             MyPersonaje(personaje)
         }
     }
 }
-@Composable
-fun ImagenPersonaje(name : String){
-    Image(
-        painter = painterResource(id = R.drawable.logo_dragonballapi),
-        contentDescription = "Android logo",
-        modifier = Modifier.padding(8.dp)
-            .size(100.dp)
-            .clip(CircleShape)
-            .background(MaterialTheme.colorScheme.primary)
-    )
-}
+//@Composable
+//fun ImagenPersonaje(name : String){
+//    Image(
+//        painter = painterResource(id = R.drawable.logo_dragonballapi),
+//        contentDescription = "Android logo",
+//        modifier = Modifier.padding(8.dp)
+//            .size(100.dp)
+//            .clip(CircleShape)
+//            .background(MaterialTheme.colorScheme.primary)
+//    )
+//}
 @Composable
 fun MyPersonaje(personaje:PersonajeTarjeta){
-Row(){
-    ImagenPersonaje(personaje.title.lowercase())
-    Personajes(personaje)
-}
-}
-@Composable
-fun Personajes(personaje:PersonajeTarjeta){
-    Column{
-        Personaje(personaje.title)
-        Personaje(personaje.body)
-    }
-}
-@Composable
-fun Personaje(datos:String){
-    Text(datos)
-}
-@Composable
-fun SaludoCard(name:String,datosCard:String) {
-    Row {
-        Image(
-            painter = painterResource(id = R.drawable.logo_dragonballapi),
-            contentDescription = "Android logo",
-            modifier = Modifier.padding(8.dp)
-                .size(100.dp)
-                .clip(CircleShape)
-                .background(MaterialTheme.colorScheme.primary)
-        )
-        Column {
-            Text(text = "Nombre $name",
-                )
+    Card(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth(),
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
+        shape = RoundedCornerShape(16.dp),
+        colors = CardDefaults.cardColors(containerColor = Color.White)
 
-            Spacer(modifier = Modifier.padding())
-            Text(text = "DatosCard $datosCard")
-
+    ) {
+        Row(
+        modifier = Modifier.padding(8.dp)
+            .background(MaterialTheme.colorScheme.background)
+        ){
+            ImagenHeroe(personaje.title.lowercase())
+            Personajes(personaje)
         }
     }
 }
 
-@Preview
 @Composable
-fun PreviewSaludoCard(){
-    Column {
-        SaludoCard("Android","persona")
+fun Personaje(name:String,color:Color,style:TextStyle,lines:Int=Int.MAX_VALUE){
+    Text(text = name,color = color, style=style,maxLines = lines)
+}
 
+@Composable
+fun Personajes(personaje:PersonajeTarjeta){
+    var expanded by remember {mutableStateOf(false)}
+    Column(
+        modifier = Modifier.padding(start = 8.dp).clickable{
+            expanded = !expanded
+        }
+    ){
+        Personaje(personaje.title,
+            MaterialTheme.colorScheme.primary,
+            MaterialTheme.typography.headlineMedium)
 
+        Personaje(personaje.body,
+            MaterialTheme.colorScheme.onBackground,
+            MaterialTheme.typography.bodyLarge,
+            if (expanded) Int.MAX_VALUE else 1
+        )
     }
 }
+
+@Composable
+fun ImagenHeroe(imageName: String){
+    val context = LocalContext.current
+    val ImageResId = remember(imageName){
+        context.resources.getIdentifier(imageName.lowercase(),
+            "drawable",context.packageName)
+    }
+    Image(
+        painter = painterResource(id=ImageResId),
+        contentDescription = null,
+        modifier = Modifier
+            .padding(16.dp)
+            .size(100.dp)
+            .clip(CircleShape)
+            .background(MaterialTheme.colorScheme.tertiary)
+    )
+}
+
+//@Composable
+//fun SaludoCard(name:String,datosCard:String) {
+//    Row {
+//        Image(
+//            painter = painterResource(id = R.drawable.logo_dragonballapi),
+//            contentDescription = "Android logo",
+//            modifier = Modifier.padding(8.dp)
+//                .size(100.dp)
+//                .clip(CircleShape)
+//                .background(MaterialTheme.colorScheme.primary)
+//        )
+//        Column {
+//            Text(text = "Nombre $name",
+//                )
+//
+//            Spacer(modifier = Modifier.padding())
+//            Text(text = "DatosCard $datosCard")
+//
+//        }
+//    }
+//}
